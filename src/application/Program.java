27db9f93b007a34e73a355c9df89358ba0ed807c
GigaDale <matheusjvs_@hotmail.com>
2022-08-01 20:12:55 -0300

    ChessSystem project

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,10 +11,6 @@ import java.util.Scanner;
 public class Program {
     public static void main(String[] args) {
 
-        //Position position = new Position(3, 5);
-        //System.out.println(position);
-
-        //Board board = new Board(8, 8);
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
 
@@ -28,7 +24,6 @@ public class Program {
             ChessPosition target = UI.readChessPosition(sc);
 
             ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-            //alooo
         }
 
     }
