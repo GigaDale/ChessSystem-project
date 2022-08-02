1ccd037c0ea55697de8cc71c5e28ef73020f7770
GigaDale <matheusjvs_@hotmail.com>
2022-08-02 00:53:17 -0300

    ChessSystem project

--- src/application/UI.java
+++ src/application/UI.java
@@ -10,36 +10,19 @@ import java.util.Scanner;
 public class UI {
 
     public static final String ANSI_RESET = "\u001b[0m";
-    public static final String ANSI_BLACK = "\u001b[30m";
-    public static final String ANSI_RED = "\u001b[31m";
-    public static final String ANSI_GREEN = "\u001b[32m";
     public static final String ANSI_YELLOW = "\u001b[33m";
-    public static final String ANSI_BLUE = "\u001b[34m";
-    public static final String ANSI_PURPLE = "\u001b[35m";
-    public static final String ANSI_CYAN = "\u001b[36m";
     public static final String ANSI_WHITE = "\u001b[37m";
-
-    public static final String ANSI_BLACK_BACKGROUND = "\u001b[40m";
-    public static final String ANSI_RED_BACKGROUND = "\u001b[41m";
-    public static final String ANSI_GREEN_BACKGROUND = "\u001b[42m";
-    public static final String ANSI_YELLOW_BACKGROUND = "\u001b[43m";
-    public static final String ANSI_BLUE_BACKGROUND = "\u001b[44m";
-    public static final String ANSI_PURPLE_BACKGROUND = "\u001b[45m";
-    public static final String ANSI_CYAN_BACKGROUND = "\u001b[46m";
-    public static final String ANSI_WHITE_BACKGROUND = "\u001b[47m";
 
     public static ChessPosition readChessPosition(Scanner sc){
         try {
             String s = sc.nextLine();
-            char column = sc.next().charAt(0);
+            char column = s.charAt(0);
             int row = Integer.parseInt(s.substring(1));
             return new ChessPosition(column, row);
         }
         catch (RuntimeException e) {
             throw new InputMismatchException("Error read ChessPosition. Valid values are from a1 to h8");
         }
-
-
     }
 
     public static void printBoard(ChessPiece[][] pieces){
@@ -59,10 +42,10 @@ public class UI {
         }
         else{
             if (piece.getColor() == Color.WHITE){
-                System.out.println(ANSI_WHITE + piece + ANSI_RESET);
+                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
             }
             else {
-                System.out.println(ANSI_YELLOW + piece + ANSI_RESET);
+                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
             }
         }
         System.out.print(" ");
