3c28f01a9271353e5d7e5635ebe0670aa43eb799
GigaDale <matheusjvs_@hotmail.com>
2022-08-04 12:22:49 -0300

    ChessSystem project

--- src/boardGame/Position.java
+++ src/boardGame/Position.java
@@ -30,4 +30,9 @@ public class Position {
     public String toString() {
         return row + ", " + column;
     }
+
+    public void setValue(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
 }
